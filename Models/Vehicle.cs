6adf3752de29ec0263c26f3fcda66848fda3b77f
Rueben Pakola Monsen <ruebenmonsen@gmail.com>
2021-08-18 15:06:08 +0200

    Added a vehicle class

---
+++ Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0.Models
+{
+    public class Vehicle
+    {
+        public int Brand { get; set; }
+    }
+}
