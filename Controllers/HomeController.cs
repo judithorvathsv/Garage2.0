3ddb42d8ccd86b711554343de1db568bd0faa5a5
Tony Berglund <tony_berglund@outlook.com>
2021-08-18 15:09:32 +0200

    Added Index2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Garage2._0.Controllers
             return View();
         }
 
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
