bf91f401f9946392df62856bd17e1b89b8332f8d
Judit Horvath <judith1horvath@outlook.com>
2021-08-19 10:05:32 +0200

    Add Properties, enum

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,28 @@ namespace Garage2._0.Models
 {
     public class Vehicle
     {
-        public int Brand { get; set; }
+        [Required]
+        public VehicleTypes VehicleType { get; set; }
+
+        [Required(ErrorMessage = "Please enter registration number!")]
+        [MaxLength(10, ErrorMessage = "Too long registration number!")]
+        [RegularExpression("^([a-zA-Z-0-9]+)$", ErrorMessage = "Invalid registration number!")]
+        public string RegistrationNumber { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Too long text for color, max length is 20 character!")]
+        public string Color { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Too long text for brand, max length is 20 character!")]
+        public string Brand { get; set; }
+
+        [MaxLength(30, ErrorMessage = "Too long text for vehicle model!")]
+        public string VehicleModel { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Please enter correct number between 0-10!")]
+        public int NumberOfWheels { get; set; }
+
+        public bool IsParked { get; set; }
+
+        public DateTime TimeOfArrival { get; set; }
     }
 }
---
+++ Models/VehicleTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0.Models
+{
+    public enum VehicleTypes
+    {
+        Car,
+        Truck,
+        Bus,
+        Tractor,
+        Motorcycle,
+        Bicycle,
+        Tricycle,
+        Tank,     
+        Boat,     
+        Airplace
+    }
+}
